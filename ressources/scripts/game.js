5c4e36e4a253ba9d809a7c98f40fdc5c91edcc81
Amani-Arnaud <karnauda1er@gmail.com>
2022-07-17 20:01:22 +0000

    correction bug LOGIQUE 17-07-2022

--- ressources/scripts/game.js
+++ ressources/scripts/game.js
@@ -2,8 +2,8 @@ $(document).ready(function() {
 
     // Click sur le bouton demarrer le quiz
     $('#btn-start').click(function() {
-        $('#quiz-box').slideDown("slow");
-        $('#intro-quiz').hide();
+        $('#quiz-box').slideDown(1000);
+        $('#intro-quiz').slideUp(2000);
     });
     // Fin bouton demarrer
 
@@ -16,8 +16,6 @@ $(document).ready(function() {
         success: function(questions) {
 
             // DECLARATION DES VARIABLES LOCALE
-            let notChoose = true;
-            let userResponse = "false"; // [par la suite] faire de cette variable un tableau qui recupere les reponses de User
             let totalPoint = 0;
             let currentQuiz = 0; // index de la question courente
             let totalQuiz = questions.length;
@@ -30,47 +28,47 @@ $(document).ready(function() {
             $('#formulaire').submit(function(e) {
                 e.preventDefault();
 
-                // si le nombre de qquestion est inferieur au total
-                if (currentQuiz < totalQuiz) {
-                    // Controle si l'utilisateur à choisir une reponse
-                    for (let i = 0; i < questions[currentQuiz].response.length; i++) {
-                        notChoose = $('#checkbox' + i).is(":checked") ? false : notChoose;
-                        userResponse = notChoose ? userResponse : $('#checkbox' + i).val();
-                    }
-                    // Fin controle
+                let chooseResponse = isChooseResponse(questions, currentQuiz);
+                let isChoose = chooseResponse[0];
+                let userResponse = chooseResponse[1];
+                // si l'index de la question est inferieur au total
+                if (currentQuiz < (totalQuiz - 1)) {
 
-                    if (notChoose) { // Si aucune reponse n'est choisi afficher un message d'erreur
-                        $('#error-message').css("color", "red").text("Choisissez une reponse !");
-                    } else { // Sinon valider le formulaire 
-                        $('#quiz-box').hide();
-                        $('#quiz-box').slideDown("slow");
+                    if (isChoose) { // Sinon valider le formulaire 
+                        $('#quiz-box').slideUp(1000).slideDown(1000);
                         currentQuiz++; // augmenter
-                        if (currentQuiz < totalQuiz) {
-                            setForm(questions[currentQuiz].question, questions[currentQuiz].response, (currentQuiz + 1)); // Afficher la prochaine question question
-                        }
 
-                        console.log(currentQuiz);
+                        setTimeout(() => {
+                            if (currentQuiz < totalQuiz) {
+                                setForm(questions[currentQuiz].question, questions[currentQuiz].response, (currentQuiz + 1)); // Afficher la prochaine question question
+                            }
+                        }, 1000);
+
                         if (currentQuiz === totalQuiz) { //Si l'index des questio est égale à total Quiz
                             $('#btn-next').val('Voir le resultat').attr("id", "btn-resume");
                         }
                         console.log(userResponse);
                         totalPoint = userResponse === "true" ? (totalPoint + 1) : totalPoint; // Si c'est une bonne reponse augmenter le total des points
-                        notChoose = true; // Nouvelle question rien n'est choisi
                         userResponse = "false"; // nouvelle question fausse reponse
                         console.log(totalPoint);
                     }
                 } else {
-                    if (totalPoint <= 5) {
-                        $('#titre').text("0" + totalPoint + "/" + totalQuiz + " c'est pas tout à fait ça...");
-                        $('#msg-result').text("Oula ! Heureusement que le Riddler est sous verrous... Il faut que vous vous repassiez les films, cette fois en enlevant peut-être le masque qui vous a bloqué la vue! Aller, rien n'est perdu !");
-                    } else if (totalPoint <= 14) {
-                        $('#titre').text(totalPoint + "/" + totalQuiz + " pas mal !");
-                        $('#msg-result').text("Encore un peu d'entraînement avec le Chevalier Noir vous serait bénéfique, mais vous pouvez marcher la tête haute, vos connaissances sont là. A vous de les consolider, foncez Gotham est votre de chasse !");
-                    } else {
-                        $('#titre').text(totalPoint + "/" + totalQuiz + " bravo !");
-                        $('#msg-result').text("Vous êtes véritablement un super fan de l'univers de Batman ! Comics, films, rien ne vous échappe. Bruce Wayne a de quoi être fier, Gotham est en paix et Batman peut prendre sa retraite, vous veillez aux grains");
+                    if (isChoose) {
+                        console.log(userResponse);
+                        totalPoint = userResponse === "true" ? (totalPoint + 1) : totalPoint; // Si c'est une bonne reponse augmenter le total des points
+                        console.log(totalPoint);
+                        if (totalPoint <= (totalQuiz / 3)) {
+                            $('#titre').text("0" + totalPoint + "/" + totalQuiz + " c'est pas tout à fait ça...");
+                            $('#msg-result').text("Oula ! Heureusement que le Riddler est sous verrous... Il faut que vous vous repassiez les films, cette fois en enlevant peut-être le masque qui vous a bloqué la vue! Aller, rien n'est perdu !");
+                        } else if (totalPoint <= (totalQuiz / 2)) {
+                            $('#titre').text(totalPoint + "/" + totalQuiz + " pas mal !");
+                            $('#msg-result').text("Encore un peu d'entraînement avec le Chevalier Noir vous serait bénéfique, mais vous pouvez marcher la tête haute, vos connaissances sont là. A vous de les consolider, foncez Gotham est votre de chasse !");
+                        } else {
+                            $('#titre').text(totalPoint + "/" + totalQuiz + " bravo !");
+                            $('#msg-result').text("Vous êtes véritablement un super fan de l'univers de Batman ! Comics, films, rien ne vous échappe. Bruce Wayne a de quoi être fier, Gotham est en paix et Batman peut prendre sa retraite, vous veillez aux grains");
+                        }
+                        $('#popup-result').css("display", "flex");
                     }
-                    $('#popup-result').css("display", "flex");
                 }
             }); // Fin
         },
@@ -78,6 +76,32 @@ $(document).ready(function() {
             console.log(questions);
         },
     });
+
+    // ###########################################################
+    // ##  Fonction de verification si user choisir une reponse ##
+    // ###########################################################
+
+    function isChooseResponse(questions, currentQuiz) {
+        // Variable locale
+        let notChoose = true;
+        let userResponse = "false"; // [par la suite] faire de cette variable un tableau qui recupere les reponses de User
+
+        // Controle si l'utilisateur à choisir une reponse
+        for (let i = 0; i < questions[currentQuiz].response.length; i++) {
+            if ($('#checkbox' + i).is(":checked")) {
+                notChoose = false;
+                userResponse = $('#checkbox' + i).val();
+            }
+        }
+        // Fin controle
+
+        if (notChoose) { // Si aucune reponse n'est choisi afficher un message d'erreur
+            $('#error-message').css("color", "red").text("Choisissez une reponse !");
+            return [false, userResponse];
+        } else { // Sinon valider le formulaire 
+            return [true, userResponse];
+        }
+    }
 
     // #########################################
     // ##  Fonction de changement de question ##
